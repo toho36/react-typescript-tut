3d4c10a914fe1fdbff02588a4c7c35208e1d93f4
toho36 <toho0036@gmail.com>
2023-07-21 16:44:18 +0200

    UseReducer increment and decrement

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+type ACTIONTYPE =
+  | { type: 'increment'; payload: number }
+  | { type: 'decrement'; payload: number };
+
+function reducer(state: { count: number }, action: ACTIONTYPE) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + action.payload };
+    case 'decrement':
+      return { count: state.count - action.payload };
+    default:
+      throw new Error();
+  }
+}
 
 function App() {
-  const [count, setCount] = useState<number | null>(null);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   return (
     <div className="App">
-      <button onClick={() => setCount((count || 0) + 1)}>Add</button>
-      <h2>{count}</h2>
+      <button onClick={() => dispatch({ type: 'increment', payload: 1 })}>
+        Add
+      </button>
+      <button onClick={() => dispatch({ type: 'decrement', payload: 1 })}>
+        Minus
+      </button>
+      {state.count}
     </div>
   );
 }
